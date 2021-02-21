9c987ad01a817563c73f55c46d82a432acd53f83
KangWon LEE <kangwon.lee@kpu.ac.kr>
2021-02-21 18:09:52 +0900

    trying to list import modules

--- test_factorial.py
+++ test_factorial.py
@@ -1,3 +1,5 @@
+import ast
+import collections
 import math
 import random
 import sys
@@ -14,7 +16,7 @@ def int_n() -> int:
     return random.randint(5, 20)
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def filename() -> str:
     return 'main.py'
 
@@ -29,6 +31,41 @@ def test_factorial_result(int_n:int):
 
 def test_is_numpy_installed():
     assert 'numpy' not in sys.modules
+
+
+def get_imports(filename):
+    # https://stackoverflow.com/a/9049549
+
+    Import = collections.namedtuple("Import", ["module", "name", "alias"])
+
+    with open(filename, 'rt') as f:
+        root = ast.parse(f.read(), filename)
+    
+    for node in ast.iter_child_nodes(root):
+        if isinstance(node, ast.Import):
+            module = []
+        elif isinstance(node, ast.ImportFrom):
+            module = node.module.split('.')
+        else:
+            continue
+
+        for n in node.names:
+            yield Import(module, n.name.split('.'), n.asname)
+
+
+@pytest.fixture(scope="session")
+def imports(filename):
+    return list(get_imports(filename))
+
+
+def test_main_imports_math(imports):
+    for import_tuple in imports:
+        assert 'math' not in import_tuple.module, "Check if main.py imports math module"
+
+
+def test_main_imports_numpy(imports):
+    for import_tuple in imports:
+        assert 'numpy' not in import_tuple.module, "Check if main.py imports numpy module"
 
 
 if "__main__" == __name__:
