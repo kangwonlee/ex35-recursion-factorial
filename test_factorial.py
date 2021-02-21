3294d6934bd081f9123ccc277d3f775d52e1c4d9
KangWon LEE <kangwon.lee@kpu.ac.kr>
2021-02-21 17:43:44 +0900

    test result type & value

--- test_factorial.py
+++ test_factorial.py
@@ -1,5 +1,6 @@
 import math
 import random
+import sys
 
 import pytest
 
@@ -9,13 +10,22 @@ random.seed()
 
 
 @pytest.fixture
-def int_n():
+def int_n() -> int:
     return random.randint(5, 20)
 
 
-def test_factorial_type(int_n):
+@pytest.fixture
+def filename() -> str:
+    return 'main.py'
+
+
+def test_factorial_type(int_n:int):
     assert isinstance(main.factorial(int_n), (int, float)), f"Check the type of the return value : {type(main.factorial(int_n))}"
 
 
-def test_factorial_result(int_n):
+def test_factorial_result(int_n:int):
     assert main.factorial(int_n) == math.factorial(int_n), f"Check the return value : {main.factorial(int_n)}"
+
+
+if "__main__" == __name__:
+    pytest.main()
